c93fa2d421cc8f216bbb729caab180d7f342dd10
Konnotakuto <s2522013@stu.musashino-u.ac.jp>
2025-11-28 16:33:41 +0900

    logout.html完成？

---
+++ simple-todo/logout.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+require_once 'config.php';
+
+// セッション変数をすべて削除
+$_SESSION = [];
+
+// セッションクッキーも削除
+if (ini_get('session.use_cookies')) {
+	$params = session_get_cookie_params();
+	setcookie(
+		session_name(),
+		'',
+		time() - 42000,
+		$params['path'],
+		$params['domain'],
+		$params['secure'],
+		$params['httponly']
+	);
+}
+
+session_destroy();
+
+$redirectUrl = defined('BASE_URL') ? BASE_URL . '/index.php' : 'index.php';
+header('Location: ' . $redirectUrl);
+exit;
